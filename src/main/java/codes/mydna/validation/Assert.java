40ba86a6fe142df8d891b4c4a08d2228ef506b45
MusicDin <dm6067@student.uni-lj.si>
2020-12-20 15:28:50 +0100

    Assert not null helper class

--- src/main/java/codes/mydna/validation/Assert.java
+++ src/main/java/codes/mydna/validation/Assert.java
@@ -1,2 +1,38 @@
-package codes.mydna.validation;public class Assert {
+package codes.mydna.validation;
+
+import codes.mydna.exceptions.NullInputException;
+
+public class Assert {
+
+    /**
+     * Throws Rest exception if object is not null
+     * @param objectClass - Object class
+     * @param o - Object to be checked
+     * */
+    public static void objectNotNull(Class<?> objectClass,  Object o){
+        if(o == null)
+            throw new NullInputException(objectClass);
+    }
+
+    /**
+     * Throws Rest exception if field is not null
+     * @param objectClass - Object class
+     * @param field - Object to be checked
+     * @param fieldName - Name of field
+     * */
+    public static void fieldNotNull(Class<?> objectClass, Object field, String fieldName){
+        if(field == null)
+            throw new NullInputException(objectClass, fieldName);
+    }
+
+    /**
+     * Checks whether field is not null
+     * @param field - Object to be checked
+     * @param fieldName - Name of field
+     * */
+    public static void fieldNotNull(Object field, String fieldName){
+        if(field == null)
+            throw new NullInputException(fieldName);
+    }
+
 }
