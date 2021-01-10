e7bde7809d44ca0bab0cd68111b7290be5bcc7ae
MusicDin <dm6067@student.uni-lj.si>
2021-01-10 21:52:46 +0100

    Added realm roles

---
+++ src/main/java/codes/mydna/auth/RealmRole.java
@@ -0,0 +1,9 @@
+package codes.mydna.auth;
+
+public class RealmRole {
+
+    public static final String ADMIN = "mdc_admin";
+    public static final String USER = "mdc_user";
+    public static final String PRO_USER = "mdc_pro_user";
+
+}
